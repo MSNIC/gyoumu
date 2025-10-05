bcbe9ab868bd9d1fc0e797250e46d6db1575facd
49372s <kyouyouidsiyou@gmail.com>
2025-10-05 15:11:12 +0900

    initialize

---
+++ .config/example.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DATABASE_ADDRESS', 'localhost:3306');
+define('DATABASE_USER', 'root');
+define('DATABASE_PASSWORD', 'root');
+define('DATABASE_NAME', 'gyoumu');
+define('DATABASE_CHARSET', 'utf8');
+
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!--
+MSNIC業務管理システム v1.0.0
+Copyright (c) 2025 MSNIC. All rights reserved.
+-->
+<!DOCTYPE html>
+<html lang="ja-jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>業務管理</title>
+</head>
+<body>
+</body>
+</html>
